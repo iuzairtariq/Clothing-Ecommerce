8d505454603c89013750f1c24da3276880ba940e
Muhammad Uzair <uzairtariq102@gmail.com>
2025-05-17 20:19:22 +0500

    updated

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -27,7 +27,7 @@ const SearchBar = () => {
 
     return showSearch && visible ? (
         <div className='flex items-center justify-center border bg-gray-100 dark:bg-gray-900 text-center rounded-full'>
-            <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-2 sm:my-4 mx-3 rounded-full w-3/4 sm:w-1/2'>
+            <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-2 mx-3 rounded-full w-3/4 sm:w-1/2'>
                 <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search' />
                 <SearchIcon />
             </div>
